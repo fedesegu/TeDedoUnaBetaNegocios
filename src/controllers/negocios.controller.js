920cf13db95124b810edc0e286b2cbf2549ba7c4
Federico Segú <federicosegu@gmail.com>
2024-07-17 16:13:41 -0300

    modificando negocios

--- src/controllers/negocios.controller.js
+++ src/controllers/negocios.controller.js
@@ -1,4 +1,4 @@
-import {getNegociosById} from "../services/negocios.service";
+import {getNegociosById, create} from "../services/negocios.service";
 
 export const getNegociosByids = async (req, res) => {
         try {
@@ -6,10 +6,19 @@ export const getNegociosByids = async (req, res) => {
             if (!userid) {
                 return res.status(400).json({ message: "user_id are required" });
             }
-            const negocio = await getNegociosByid(user_id);
+            const negocio = await getNegociosById(user_id);
             res.json(negocio);
         } catch (err) {
             console.error(err.message);
             res.status(500).send('Server error');
         }
     };
+    export const createNegocio = async (req, res) =>{
+        try {
+           const {userid_negocios, name_negocios} = req.body
+           const negocio = await create (userid_negocios, name_negocios);
+           res.status(201).json(negocio);
+        } catch (error) {
+            
+        }
+    }
