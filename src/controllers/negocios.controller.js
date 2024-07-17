b487312f2fdff62933c37634d0e492e24c958ac9
Federico Segú <federicosegu@gmail.com>
2024-07-17 20:52:10 -0300

    agregando funcion get de negocio en controlador

--- src/controllers/negocios.controller.js
+++ src/controllers/negocios.controller.js
@@ -1,4 +1,5 @@
-import {getNegociosById, create} from "../services/negocios.service.js";
+import {getNegociosById, create, getUserBusinesses} from "../services/negocios.service.js";
+import jwt from "jsonwebtoken";
 
 export const getNegociosByids = async (req, res) => {
         try {
@@ -23,3 +24,32 @@ export const getNegociosByids = async (req, res) => {
         res.status(500).send('Server error');
         }
     }
+
+
+export const getUserBusinessesController = async (req, res) => {
+  try {
+    // Verificar y decodificar el token JWT de las cookies
+    const token = req.cookies.token;
+    if (!token) {
+      return res.status(401).json({ message: 'Authentication token is missing' });
+    }
+
+    // Decodificar el token JWT para obtener el userId
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    const userId = decoded.id;
+
+    // Llamar a la función getUserBusinesses con el userId extraído
+    const businesses = await getUserBusinesses(userId);
+
+    // Devolver la lista de negocios en la respuesta
+    return res.status(200).json(businesses);
+  } catch (err) {
+    // Manejar errores, incluyendo errores de autenticación
+    if (err.name === 'JsonWebTokenError') {
+      return res.status(401).json({ message: 'Invalid token' });
+    }
+    console.error(err);
+    return res.status(500).json({ message: 'Internal server error' });
+  }
+};
+
--- src/services/negocios.service.js
+++ src/services/negocios.service.js
@@ -1,9 +1,9 @@
 import pool from "../DB/configDB.js";
 
-export const getNegociosById = async () => {
-    const [rows] = await pool.query("SELECT * FROM negocios WHERE userid_negocios = ?", [userid_negocios, name_negocios]);
-    return rows;
-}
+// export const getNegociosById = async () => {
+//     const [rows] = await pool.query("SELECT * FROM negocios WHERE userid_negocios = ?", [userid_negocios, name_negocios]);
+//     return rows;
+// }
 export const create = async ( name_negocio) => {
     try {
         const query = "INSERT INTO negocio (name_negocio) VALUES (?)";
@@ -13,3 +13,18 @@ export const create = async ( name_negocio) => {
         throw new Error('Error creating negocio: ' + err.message);
     }
 };
+
+export const getUserBusinesses = async (userId) => {
+    try {
+      const [rows, fields] = await pool.execute(`
+        SELECT n.id_negocio, n.name_negocio
+        FROM asignaciones a
+        JOIN negocio n ON a.negocioid_asignaciones = n.id_negocio
+        WHERE a.userid_asignaciones = ?
+      `, [userId]);
+      return rows;
+    } catch (err) {
+      console.error(err);
+      throw err;
+    }
+  };
